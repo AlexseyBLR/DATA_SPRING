815755909cc87b374c87028d8a47700f9e4c9e88
Alexey <meleschenya1997@gmail.com>
2018-07-30 00:00:40 +0300

    login(validation), add user

--- src/main/java/com/compit/spring_data/service/TestServiceImpl.java
+++ src/main/java/com/compit/spring_data/service/TestServiceImpl.java
@@ -4,10 +4,12 @@ import com.compit.spring_data.dao.TestDAO;
 import com.compit.spring_data.entity.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class TestServiceImpl implements TestService {
 
     @Autowired
